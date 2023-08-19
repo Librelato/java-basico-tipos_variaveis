aa46232bc30ef8266c10d94712b47424d7d6fb98
Gefferson Rodrigo Librelato <Librelato@users.noreply.github.com>
2023-08-19 16:28:34 -0300

    Update App.java
    xxx

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!"); //este é um comentário inserido no git
     }
 }
