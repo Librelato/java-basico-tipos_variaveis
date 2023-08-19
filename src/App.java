c69678cada4174a54ddfb7a228d7dcabffd6efc3
Gefferson Librelato <gefferson.librelato@gmail.com>
2023-08-19 16:35:11 -0300

    inserido nova linha

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!"); //este é um comentário inserido no git
+        System.out.println("Teste de nova linha!!!");
     }
 }
